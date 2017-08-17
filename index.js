6464dc458d9e236ce1e7461cbcd01d2c624d3c61
luisarita <github@luisarita.com>
2017-08-17 12:15:46 -0400

    Done.

--- index.js
+++ index.js
@@ -1,22 +1,71 @@
 function getIssues() {
+  const repo = 'luisarita/javascript-fetch-lab'
+  const url = `https://api.github.com/repos/${repo}/issues`
+
+  fetch(url)
+  .then(resp => resp.json())
+  .then(json => console.log(json));
 }
 
 function showIssues(json) {
+  var issues = {
+    //name: json.name,
+    //html_url: json.html_url,
+    body: ''
+  }
+
+  var template = Handlebars.compile(document.getElementById("issues-template").innerHTML);
+  var html = template(issues);
 }
 
 function createIssue() {
+  //const repo = 'luisarita/javascript-fetch-lab-web-062617'
+  const repo = 'luisarita/javascript-fetch-lab'
+  const url = `https://api.github.com/repos/${repo}/issues`
+  const title = document.getElementById("title").value
+  const body = document.getElementById("body").value
+
+  fetch(url, {
+    method: 'POST',
+    body: {
+      title: title,
+      body: body
+    },
+    headers: {
+      Authorization: `token ${getToken()}`
+    }
+  })
+  .then(res => res.json())
+  .then(json => showIssues(json))
 }
 
 function showResults(json) {
+  if (json == undefined){
+    json = {name: "TEST", html_url: "https://api.github.com/"}
+  }
+  var repo = {
+    name: json.name,
+    html_url: json.html_url,
+  }
+
+  var template = Handlebars.compile(document.getElementById("repo-template").innerHTML);
+  var html = template(repo);
 }
 
 function forkRepo() {
   const repo = 'learn-co-curriculum/javascript-fetch-lab'
-  //use fetch to fork it!
+  const url = `https://api.github.com/repos/${repo}/forks`
+
+  fetch(url, {
+    method: 'POST',
+    headers: {
+      Authorization: `token ${getToken()}`
+    }
+  })
+  .then(res => res.json())
+  .then(json => showResults(json))
 }
 
 function getToken() {
-  //change to your token to run in browser, but set
-  //back to '' before committing so all tests pass
   return ''
 }
